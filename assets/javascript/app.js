69d5339ff85931ee82e8feba77ac312a43150f5e
cgenel <genel661@gmail.com>
2020-02-03 16:17:53 -0800

    initialized firebase database

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDzcpwqHw3hvDKV6-gls22x8oEhlrx_NAY",
+  authDomain: "train-schedule-4641c.firebaseio.com",
+  databaseURL: "https://train-schedule-4641c.firebaseio.com/",
+  projectId: "train-schedule-4641c",
+  storageBucket: "gs://train-schedule-4641c.appspot.com"
+};
